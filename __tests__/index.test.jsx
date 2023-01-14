9ad208b0d715cf8438f3e298fae401898a8ee9f0
Sergio Kopplin <sergiokopplin@gmail.com>
2023-01-14 14:10:40 -0300

    chore: adds testing tools

---
+++ __tests__/index.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../src/pages/home";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Home/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
