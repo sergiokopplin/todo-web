f9feff0739c72f21f6de4f493b977cafba82827d
Sergio Kopplin <sergiokopplin@gmail.com>
2023-01-25 19:43:09 -0300

    feat: adds missing stories

---
+++ src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Heading } from "./Heading";
+
+export default {
+  title: "Components/Heading",
+  component: Heading,
+} as ComponentMeta<typeof Heading>;
+
+const Template: ComponentStory<typeof Heading> = (args) => (
+  <Heading {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  children: "QUARTA-FEIRA",
+};
